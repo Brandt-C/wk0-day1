50f38f21510b570f92af8339d654695e61229613
Brandt Carlson <brandt.carlson11@gmail.com>
2023-08-22 21:05:23 -0500

    more python!

---
+++ cw.py
@@ -0,0 +1,15 @@
+num1 = 9
+num2 = 9
+mult_num = num1 * num2
+print(mult_num)
+print(9*9)
+
+def mult(number1, number2):
+    print(number1 * number2)
+
+def mult2(n1, n2):
+    return n1 * n2
+
+
+mult(9, '9')
+
